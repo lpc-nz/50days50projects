14a11d6eca39fbe1eff1b09f9a685069313c5c88
Chung Le <lpc-nz@users.noreply.github.com>
2021-04-01 12:45:26 +1300

    setup the API key

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -1,1 +1,7 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bfb66de0442d1847341bc85021136ebd&page=1';
 
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=bfb66de0442d1847341bc85021136ebd&query="';
